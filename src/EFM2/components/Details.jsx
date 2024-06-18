2a12bc1c613930577281303d4c49d5ff0d8181fb
assadyoussef0 <amine150assad@gmail.com>
2024-06-18 11:35:08 +0100

    complet details

--- src/EFM2/components/Details.jsx
+++ src/EFM2/components/Details.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function Details() {
+  const [data,setData]=useState([]);
+  const fetchApiBook = async () => {
+    try {
+        const res = await axios.get('https://retoolapi.dev/WDlW2j/data');
+        setData(res.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+useEffect(() => {
+    fetchApiBook();
+    // console.log(data);
+});
   return (
-    <div>Details</div>
+    <div className='container m-5 d-flex justify-content-center align-items-center' >
+      <table className="table table-dark table-striped">
+     
+        <thead>
+          <tr>
+            <th scope="col">ISBN</th>
+            <th scope="col">Titre</th>
+            <th scope="col">Genre</th>
+            <th scope="col">PRix</th>
+          </tr>
+        </thead>
+        <tbody>
+           {
+            data.map((d,i)=>(
+              <tr key={i} >
+                <td> {d.ISBN} </td>
+                <td> {d.Title} </td>
+                <td> {d.Genre} </td>
+                <td> {d.Prix} </td>
+              </tr>
+            ))
+           }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
